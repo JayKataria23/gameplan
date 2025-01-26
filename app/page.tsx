eefc48c2fed0965206cac51096aa9bf76e01c578
jay_kataria <jaykataria2004@gmail.com>
2025-01-26 16:53:08 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
+import GameSelectionPage from "@/components/GameSelectionPage";
 import Hero from "@/components/hero";
 
 export default async function Home() {
   return (
     <>
       <Hero />
+      <GameSelectionPage />
     </>
   );
 }
